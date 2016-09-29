018363dd40646cfb3b6463e6188449bb77400495
Riekelt Brands <riekelt@skrepr.com>
2016-09-29 16:48:28 +0200

    Array source

--- src/Datasource/ArraySource.php
+++ src/Datasource/ArraySource.php
@@ -48,7 +48,7 @@ class ArraySource extends AbstractSource
 
         foreach ($array as $item) {
             foreach ($item as $value) {
-                if (stripos($value, $term)) {
+                if (stripos($value, $term) !== false) {
                     $results[] = $item;
                 }
             }
